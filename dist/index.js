f5c0aa4200065822ffdf3485a55541c1782a1cbd
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-03 15:52:59 -0700

    implement socket.io - followed their getting started tut

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
+const socket_io_1 = require("socket.io");
 const app = (0, express_1.default)();
 const port = 3000;
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server);
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path_1.default.join(__dirname, '/temporaryClientTesting.html'));
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`); // eslint-disable-line no-console
+io.on('connection', (socket) => {
+    console.log('a user connected'); // eslint-disable-line no-console
+    socket.on('emit', (uid) => {
+        console.log(`recieved this user id: ${uid}`); // eslint-disable-line no-console
+        io.emit('notification', 'a user has joined the socket');
+    });
+    socket.on('disconnect', () => {
+        console.log('a user disconnected'); // eslint-disable-line no-console
+    });
+});
+server.listen(port, () => {
+    console.log(`listening on port *:${port}`); // eslint-disable-line no-console
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express from 'express';
+import http from 'http';
+import path from 'path';
+import { Server } from 'socket.io';
 
 const app = express();
 const port = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '/temporaryClientTesting.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`); // eslint-disable-line no-console
+io.on('connection', (socket) => {
+  console.log('a user connected'); // eslint-disable-line no-console
+
+  socket.on('emit', (uid: string) => {
+    console.log(`received this user id: ${uid}`); // eslint-disable-line no-console
+
+    io.emit('notification', 'a user has joined the socket');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected'); // eslint-disable-line no-console
+  });
+});
+
+server.listen(port, () => {
+  console.log(`listening on port *:${port}`); // eslint-disable-line no-console
 });
