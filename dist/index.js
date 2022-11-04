88214a209d5532d26cbab7d15b915635831a9642
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-03 17:00:00 -0700

    implement basic queue and matching system

--- dist/index.js
+++ dist/index.js
@@ -11,14 +11,33 @@ const app = (0, express_1.default)();
 const port = 3000;
 const server = http_1.default.createServer(app);
 const io = new socket_io_1.Server(server);
+const queue = {};
+function deleteFromQueueAndDisconnect(uid) {
+    queue[uid].disconnect();
+    delete queue[uid];
+}
+function match(uid1, uid2) {
+    console.log(`matching ${uid1} and ${uid1}`);
+    io.to(queue[uid1].id).emit('match-found', `matched with ${uid2}`);
+    io.to(queue[uid2].id).emit('match-found', `matched with ${uid1}`);
+    deleteFromQueueAndDisconnect(uid1);
+    deleteFromQueueAndDisconnect(uid2);
+}
+function tryFindMatch(q) {
+    const uids = Object.keys(q);
+    if (uids.length >= 2) {
+        match(uids[0], uids[1]);
+    }
+}
 app.get('/', (req, res) => {
     res.sendFile(path_1.default.join(__dirname, '/temporaryClientTesting.html'));
 });
 io.on('connection', (socket) => {
     console.log('a user connected'); // eslint-disable-line no-console
-    socket.on('emit', (uid) => {
-        console.log(`recieved this user id: ${uid}`); // eslint-disable-line no-console
-        io.emit('notification', 'a user has joined the socket');
+    socket.on('add-uid-to-queue', (uid) => {
+        queue[uid] = socket;
+        tryFindMatch(queue);
+        console.log('queue:', Object.keys(queue));
     });
     socket.on('disconnect', () => {
         console.log('a user disconnected'); // eslint-disable-line no-console
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,36 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const port = 3000;
 const server = http.createServer(app);
 const io = new Server(server);
+
+type Queue = { [uid: string]: Socket };
+
+const queue: Queue = {};
+
+function deleteFromQueueAndDisconnect(uid: string) {
+  queue[uid].disconnect();
+  delete queue[uid];
+}
+
+function match(uid1: string, uid2: string) {
+  console.log(`matching ${uid1} and ${uid1}`);
+  io.to(queue[uid1].id).emit('match-found', `matched with ${uid2}`);
+  io.to(queue[uid2].id).emit('match-found', `matched with ${uid1}`);
+  deleteFromQueueAndDisconnect(uid1);
+  deleteFromQueueAndDisconnect(uid2);
+}
+
+function tryFindMatch(q: Queue) {
+  const uids = Object.keys(q);
+  if (uids.length >= 2) {
+    match(uids[0], uids[1]);
+  }
+}
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/temporaryClientTesting.html'));
@@ -15,10 +39,9 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('a user connected'); // eslint-disable-line no-console
 
-  socket.on('emit', (uid: string) => {
-    console.log(`received this user id: ${uid}`); // eslint-disable-line no-console
-
-    io.emit('notification', 'a user has joined the socket');
+  socket.on('add-uid-to-queue', (uid: string) => {
+    queue[uid] = socket;
+    tryFindMatch(queue);
   });
 
   socket.on('disconnect', () => {
